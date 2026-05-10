744ed4ab56709bb87c3ec4896bb374c54ef16b58
Mariam-235 <mariamelshazly04@gmail.com>
2026-05-10 05:25:09 +0300

    Add Footer component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,6 @@
-import Navbar from "../components/Navbar/Navbar";
-
 function Home() {
   return (
     <div>
-      <Navbar />
-
       <h1 className="text-center mt-5">
         Welcome to Bloom 🌸
       </h1>
